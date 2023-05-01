e28b3fe8dc8507432ebbbacae6c11e206dbe6417
Renato Aurelio <viaraco21@gmail.com.br>
2023-05-01 12:52:11 -0300

    Inclusão do volume no projeto

--- app/index.js
+++ app/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const app = express();
 
+
+
 #### no espaço em branco é onde vc vai colocar a sua aplicação
+
+
+import express from "express"
+import livros from "./livrosRouters.js"
+
+const routes = (app) => {
+  app.route('/').get((req, res) => {
+    res.status(200).send({titulo: "Curso de node"})
+  }) 
+  app.use(
+    express.json(),
+    livros
+  )
+
+  }
+export default routes
+
+############################################
 
 const port = process.env.PORT || 3000;
 
