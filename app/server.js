b5dc994071c6977d536a501a9ab9eee80fbc4ac6
Renato Aurelio <viaraco21@gmail.com.br>
2023-05-03 14:24:40 -0300

    Ajustes no server.js  do projeto add a biblioteca Nodemon

--- app/server.js
+++ app/server.js
@@ -1,10 +1,17 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+  '/': 'Projeto-argocd',
+  '/ADM': 'Inclui Exclui usuario senha',
+  '/SFTP': 'Listagem de USUARIOS'
+  }
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Projeto-argocd');
-})
+    res.end(rotas[req.url]);
+  })
+
 server.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`)
 })
