a33d0d642292fcf9ef348d86f46333f223d63145
Renato Aurelio <viaraco21@gmail.com.br>
2023-05-03 16:04:49 -0300

    Criação do metodo GET / PUSH no app

--- app/src/app.js
+++ app/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const usuario = [
   {id: 1, "usuario": "raco"},
   {id: 2, "usuario": "raco2023@"}
@@ -13,6 +15,11 @@ app.get('/', (req, res) => {
 
 app.get('/usuario', (req, res) => {
   res.status(200).json(usuario)
+})
+
+app.post('/usuario', (req, res) => {
+    usuario.push(req.body);
+    res.status(201).send('usuario cadastrado com sucesso')
 })
 
 export default app
