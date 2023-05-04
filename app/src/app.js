c0de0aa8c042b414cd32a536269220ef88c54145
Renato Aurelio <viaraco21@gmail.com.br>
2023-05-04 14:38:15 -0300

    Criação do metodo PUT / DELETE no app

--- app/src/app.js
+++ app/src/app.js
@@ -17,9 +17,31 @@ app.get('/usuario', (req, res) => {
   res.status(200).json(usuario)
 })
 
+app.get('/usuario/:id', (req, res) => {
+  let index = buscaUsuario(req.params.id);
+  res.json(usuario[index]);
+  })
+
 app.post('/usuario', (req, res) => {
     usuario.push(req.body);
     res.status(201).send('usuario cadastrado com sucesso')
 })
+
+app.put('/usuario/:id', (req, res) => {
+  let index = buscaUsuario(req.params.id);
+  usuario[index].usuario = req.body.usuario;
+  res.json(usuario);
+  })
+
+app.delete('/usuario/:id', (req, res) => {
+  let {id} = req.params;
+  let index = buscaUsuario(id);
+  usuario.splice(index, 1);
+  res.send(`Usuario ${id} removido com sucesso`);
+  })
+
+function buscaUsuario(id) {
+  return usuario.findIndex(usuario => usuario.id == id)
+  }
 
 export default app
